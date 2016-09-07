7ea88b57057bf09585ca5d902028580f491c8217
Chirag Sanghvi <csanghvi@appacitive.com>
2016-09-07 09:58:00 -0700

    Changed entity creation default column handling

--- model/entities.js
+++ model/entities.js
@@ -17,6 +17,29 @@ var Entities = function(options) {
 	this.catalog = this.table.catalog;
 };
 
+var create = function(entity, self) {
+	var autogenColumns = [], defer = Q.defer();
+	self.table.content.column_definitions.forEach(function(c) {
+		if (!entity.hasOwnProperty(c.name)) {
+			if (c.type.typename.indexOf('serial') === 0 && !entity.hasOwnProperty(c.name)) {
+				autogenColumns.push(c.name);
+			} else if (c.default !== null && c.default !== undefined) {
+				autogenColumns.push(c.name);
+			}
+		}
+	});
+
+	var autogenParam = (autogenColumns.length) ? ("?defaults=" + autogenColumns.join(',')) : "";
+
+	http.post(self.url + "/catalog/" + self.catalog.id + "/entity/" + self.schema.name + ":" + self.table.name + autogenParam, [entity]).then(function(response) {
+		defer.resolve(response.data);
+	}, function(err) {
+		defer.reject(err, self);
+	});
+
+	return defer.promise;
+};
+
 /**
  @param {options} A json object which should contain a list of json entities { entities: [{ .. }] }.
  * @returns {Promise} Returns a promise.
@@ -33,27 +56,22 @@ Entities.prototype.create = function(options) {
 		return defer.promise;
 	}
 
-	var autogenColumns = [];
-	this.table.content.column_definitions.forEach(function(c) {
-		if (c.type.typename.indexOf('serial') === 0 && !options.entities[0].hasOwnProperty(c.name)) {
-			autogenColumns.push(c.name);
-		} else if (c.default !== null && c.default !== undefined) {
-			autogenColumns.push(c.name);
-		}
+	var self = this, promises = [];
+	options.entities.forEach(function(e) {
+		promises.push(create(e, self));
 	});
 
-	var autogenParam = (autogenColumns.length) ? ("?defaults=" + autogenColumns.join(',')) : "";
-
-	http.post(this.url + "/catalog/" + this.catalog.id + "/entity/" + this.schema.name + ":" + this.table.name + autogenParam, options.entities).then(function(response) {
+	Q.all(promises).then(function() {
 		self.table.entityCount = options.entities.length;
-		defer.resolve(response.data);
+		defer.resolve(options.entities);
 	}, function(err) {
 		defer.reject(err, self);
 	});
 
-
 	return defer.promise;
 };
+
+
 
 /**
  *
