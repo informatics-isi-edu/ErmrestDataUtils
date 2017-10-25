7ff6ec042ac24ac1ac3c1f53c6dc3c2f139dca0d
Chirag Sanghvi <csanghvi@appacitive.com>
2017-10-25 13:08:36 -0700

    Added support to import system columns in table

---
+++ model/system_columns.js
@@ -0,0 +1,71 @@
+module.exports = [{
+    "comment": null,
+    "name": "RID",
+    "nullok": false,
+    "default": null,
+    "type": {
+      "typename": "ermrest_rid",
+      "is_domain": true,
+      "base_type": {
+        "typename": "text"
+      }
+    },
+    "annotations": {}
+  },
+  {
+    "comment": null,
+    "name": "RCT",
+    "nullok": false,
+    "default": null,
+    "type": {
+      "typename": "ermrest_rct",
+      "is_domain": true,
+      "base_type": {
+        "typename": "timestamptz"
+      }
+    },
+    "annotations": {}
+  },
+  {
+    "comment": null,
+    "name": "RMT",
+    "nullok": false,
+    "default": null,
+    "type": {
+      "typename": "ermrest_rmt",
+      "is_domain": true,
+      "base_type": {
+        "typename": "timestamptz"
+      }
+    },
+    "annotations": {}
+  },
+  {
+    "comment": null,
+    "name": "RCB",
+    "nullok": true,
+    "default": null,
+    "type": {
+      "typename": "ermrest_rcb",
+      "is_domain": true,
+      "base_type": {
+        "typename": "text"
+      }
+    },
+    "annotations": {}
+  },
+  {
+    "comment": null,
+    "name": "RMB",
+    "nullok": true,
+    "default": null,
+    "type": {
+      "typename": "ermrest_rmb",
+      "is_domain": true,
+      "base_type": {
+        "typename": "text"
+      }
+    },
+    "annotations": {}
+  }
+];
--- model/table.js
+++ model/table.js
@@ -2,6 +2,7 @@ var chance =  new (require('chance'))();
 var Q = require('q');
 var http = require('request-q');
 var utils = require('./utils.js');
+var systemColumns = require('./system_columns.js');
 
 /* @namespace Table
  * @desc
@@ -34,6 +35,30 @@ var Table = function(options) {
 	};
 };
 
+Table.prototype.addSystemColumsAndKeys = function() {
+	this.content.column_definitions = this.content.column_definitions || [];
+
+	var localSystemColumns = JSON.parse(JSON.stringify(systemColumns));
+	var localSystemCoumnnNames = localSystemColumns.map(c => c.name);
+
+	var systemColumnsFound = this.content.column_definitions.filter((c) => (localSystemCoumnnNames.indexOf(c.name) !== -1)).map(c => c.name);
+	localSystemColumns.forEach(c => {
+		if (systemColumnsFound.indexOf(c.name) === -1) {
+			this.content.column_definitions.push(c);
+		}
+	});
+
+	this.content.keys = this.content.keys || [];
+
+	var found = this.content.keys.find(k => ((k.unique_columns.legnth === 1) && (k.unique_columns.indexOf("RID") !== -1)));
+
+	if (!found) {
+		this.content.keys.push({ unique_columns: ["RID"], annotations: {} });
+	}
+
+	console.log("Table " + this.name + " has columns " + this.content.column_definitions.map(c => c.name).toString());
+};
+
 /**
  * @param {timeout} Optional : Used to add a wait between creation of table to avoid conflicts
  * @returns {Promise} Returns a promise.
@@ -48,6 +73,8 @@ Table.prototype.create = function(timeout) {
 	this.content.schema_name = this.schema.name;
 	this.foreignKeys = this.content.foreign_keys;
 	this.content.foreign_keys = [];
+
+	this.addSystemColumsAndKeys();
 
 	setTimeout(function() {
 
