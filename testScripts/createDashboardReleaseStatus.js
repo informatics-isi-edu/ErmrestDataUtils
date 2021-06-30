90460539e3105c8dc605097534c854219334637c
Joshua Chudy <jchudy@isi.edu>
2021-06-30 11:35:33 -0700

    add enumerate acl and isrd-testers to each one

--- testScripts/createDashboardReleaseStatus.js
+++ testScripts/createDashboardReleaseStatus.js
@@ -27,12 +27,19 @@ dataUtils.importData(configuration).then(function(data) {
     console.log("Please remember to clean up the catalog.");
 
     var acls = {
+        "enumerate": [
+            "https://auth.globus.org/eae2035c-01d0-11e6-a311-c78c6eaeef83", // jchudy test
+            "https://auth.globus.org/9d596ac6-22b9-11e6-b519-22000aef184d", // Anyone is "isrd-testers" can see catalog/schema ACL
+            "https://auth.globus.org/176baec4-ed26-11e5-8e88-22000ab4b42b"  // Anyone in "isrd-staff" can see catalog/schema ACL
+        ],
         "select": [
             "https://auth.globus.org/eae2035c-01d0-11e6-a311-c78c6eaeef83", // jchudy test
+            "https://auth.globus.org/9d596ac6-22b9-11e6-b519-22000aef184d", // Anyone is "isrd-testers" can read ACL
             "https://auth.globus.org/176baec4-ed26-11e5-8e88-22000ab4b42b"  // Anyone in "isrd-staff" can read ACL
         ],
         "write": [
             "https://auth.globus.org/eae2035c-01d0-11e6-a311-c78c6eaeef83", // jchudy test
+            "https://auth.globus.org/9d596ac6-22b9-11e6-b519-22000aef184d", // Anyone is "isrd-testers" can write ACL
             "https://auth.globus.org/176baec4-ed26-11e5-8e88-22000ab4b42b"  // Anyone in "isrd-staff" can write ACL
         ]
     };
