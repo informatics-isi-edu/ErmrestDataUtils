9954224535d5e86c69d7f2ab705132c22536a2bd
Joshua Chudy <jchudy@isi.edu>
2023-03-30 16:19:14 -0700

    defaults test script

---
+++ testScripts/createDefaults.js
@@ -0,0 +1,41 @@
+var dataUtils = require('./../import.js');
+var Catalog = require('./../model/catalog.js');
+
+var configuration = {
+    setup: {
+        "catalog": {},
+        "schema": {
+            "name": "defaults",
+            "createNew": true,
+            "path": "schema/defaults.json"
+        },
+        "tables": {
+            "createNew": true
+        },
+        "entities": {
+            "createNew": true,
+            "path": "data/defaults"
+        }
+    },
+    url: process.env.ERMREST_URL,
+    authCookie: process.env.AUTH_COOKIE,
+    cleanup: false
+};
+
+dataUtils.importData(configuration).then(function(data) {
+    console.log("Data imported with catalogId " + data.catalogId);
+    console.log("Please remember to clean up the catalog.");
+
+    // Anyone in "isrd-staff" can read ACL
+    return Catalog.addACLs(data.schema.url, data.catalogId, {"select": ["https://auth.globus.org/176baec4-ed26-11e5-8e88-22000ab4b42b"]});
+    // Anyone can read ACL
+    // return Catalog.addACLs(data.schema.url, data.catalogId, {"select": ["*"]});
+}, function(err) {
+    console.log("Unable to import data");
+    console.dir(err);
+}).then(function(data) {
+    console.log("ACLs set");
+}, function(err) {
+    console.log("Unable to set ACLs");
+    console.dir(err);
+});
